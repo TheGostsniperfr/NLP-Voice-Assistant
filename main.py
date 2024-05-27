7bc88d6970b59142499f5e11c2f4711a1a45f145
The_Gost_sniper <54660359+TheGostsniperfr@users.noreply.github.com>
2024-05-27 11:26:05 +0200

    ✅ Add query matching

--- main.py
+++ main.py
@@ -3,11 +3,14 @@ import pyttsx3
 import random
 from datetime import datetime
 import os
+from sklearn.feature_extraction.text import TfidfVectorizer
+import nltk
+import numpy as np
 
 
 engine = pyttsx3.init()
 voices = engine.getProperty('voices')
-engine.setProperty('voice', voices[0].id)
+engine.setProperty('voice', voices[2].id)
 
 def speak(audio):
     engine.say(audio)
@@ -17,7 +20,6 @@ def speak(audio):
 
 
 def takeCommand():
-     
     r = sr.Recognizer()
      
     with sr.Microphone() as source:
@@ -39,25 +41,55 @@ def takeCommand():
     return query
 
 
+conversation = [["Hello", "Hi"],["How are you?", "I am fine"], ["What is your name?", "My name is HAL" ]]
+#make corpus
+corpus = []
+for [q,_] in conversation:
+    corpus.append(q)
+print(corpus)   
+#make TFIDF
+corpus = [x.lower() for x in corpus]
+nltk.download('stopwords', quiet=True)
+stopwords = nltk.corpus.stopwords.words('english')
+#vectorizer = TfidfVectorizer(stop_words = stopwords )
+vectorizer = TfidfVectorizer()
+X = vectorizer.fit_transform(corpus)
+X_vect = np.array(X.todense().copy())
+#print(X_vect)
+#print(vectorizer.get_feature_names())
+#cosine similarity
+def cos_sim(v1, v2):
+    costheta = np.dot(v1,v2)/(np.linalg.norm(v1)*np.linalg.norm(v2))
+    return(costheta)
+#REPL
 while True:
-    command = takeCommand()
-    #Conversational
-    if 'how are you' in command:
-        speak("I'm doing well")
-    if 'thank you' in command:
-        speak("Anytime")
+    query =takeCommand()
 
-    #Tasks
-    if 'time' in command:
-        now = datetime.now()
-        current_time = now.strftime("%H:%M:%S")
-        speak("The Current Time is " + current_time)  
-    if 'random number' in command:
-        randInt = random.randint(0, 10) 
-        speak("A random number between 0 and 10 is " + str(randInt))
-    if 'open slack' in command:
-        os.system('open /Applications/Slack.app')
-        speak("Opening Slack")
-    if 'close slack' in command:
-        os.system('open /Applications/Slack.app')
-        speak("Closing Slack")
+    if "quit" in query:
+        break
+    q_vect = np.array(vectorizer.transform([query]).todense().copy())[0,:]
+    #print(q_vect)
+    match = [cos_sim(q_vect, v) for v in X_vect]
+    print(match)
+    #find max index
+    indexMax = max([(v,i) for i,v in enumerate(match)])
+    #print(indexMax)
+    speak(conversation[indexMax[1]][1])
+
+
+# while True:
+#     command = takeCommand()
+#     #Conversational
+#     if 'how are you' in command:
+#         speak("I'm doing well")
+#     if 'thank you' in command:
+#         speak("Anytime")
+
+#     #Tasks
+#     if 'time' in command:
+#         now = datetime.now()
+#         current_time = now.strftime("%H:%M:%S")
+#         speak("The Current Time is " + current_time)  
+#     if 'random number' in command:
+#         randInt = random.randint(0, 10) 
+#         speak("A random number between 0 and 10 is " + str(randInt))
