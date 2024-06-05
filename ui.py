fc03bcd2e0a0e63f00a92be8ce5cd91e0fa8d4ed
Lenny Chiadmi <lenny.chiadmi@gmail.com>
2024-06-05 11:28:56 +0200

    add async text

--- ui.py
+++ ui.py
@@ -1,6 +1,6 @@
 import flet as ft
-
 import asyncio
+
 async def type_text(page, text_field, text, delay=0.1):
     for char in text:
         text_field.value += char
@@ -11,13 +11,14 @@ async def type_text(page, text_field, text, delay=0.1):
 def chatting(page: ft.Page):
     page.title = "NLP Project"
     chat = ft.Column()
-    new_message = ft.TextField()
+    new_message = ft.TextField(label="Message to send", hint_text="")
 
     def send_click(e):
         chat.controls.append(ft.Text(new_message.value))
+        chat.controls.append(ft.Text(""))
         new_message.value = ""
         page.update()
-
+        asyncio.run(type_text(page, chat.controls[len(chat.controls) - 1], "Hello, it is me :p"))
     page.add(
         chat, ft.Row(controls=[new_message, ft.ElevatedButton("Send", on_click=send_click)])
     )
